633db4f5d53ab1065bfa04fd7e3834fe20385394
Serg <reddogfeet@gmail.com>
2019-03-16 10:37:01 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private EditText mText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button_s);
+        mText = findViewById(R.id.timer);
+    }
+}
